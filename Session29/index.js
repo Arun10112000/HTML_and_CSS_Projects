8d3c2a52e5cd7c7eec8a528ee404681c1338f09f
Arun <141402276+Arun10112000@users.noreply.github.com>
2023-10-24 08:21:39 +0530

    Update index.js

--- Session29/index.js
+++ Session29/index.js
@@ -4,6 +4,7 @@ const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
 const mobileNav = document.querySelector(".mobileNav");
 
+
 hamburger.addEventListener("click", () => {
   bar1.classList.toggle("animateBar1");
   bar2.classList.toggle("animateBar2");
